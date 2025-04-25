f916d78b291466822bbdc5f68c8e1d185538fbf1
이은주 <silverzoo030@gmail.com>
2025-04-25 22:19:03 +0900

    [Bronze IV] Title: 녹색거탑, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/24723. 녹색거탑/녹색거탑.java
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -1,10 +1,10 @@
-import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        System.out.println((int)Math.pow(2,n));
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+
+        System.out.print((int)Math.pow(2,N));
     }
 }
